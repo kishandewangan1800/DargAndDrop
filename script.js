36ff8daa26f19cb2ab4423f203a8912fca2ec8f2
kishandewangan1800 <91982553+kishandewangan1800@users.noreply.github.com>
2022-02-23 18:36:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 const input = document.getElementById("input");
 const button = document.getElementById("button");
+const addnewlist = document.getElementById("addlistitem")
 
 const color = ["white", "yellow", "lightgreen"]
 // const addList = document.querySelector(".main-list"); 
 
-const list = document.querySelectorAll(".list");
-let listItem = document.querySelectorAll(".list-item");
+const lists = document.querySelector(".lists");
+
+lists.addEventListener("contextmenu",(e)=>{
+    e.preventDefault();
+    if(e.target.classList.contains('list-item')){
+        let flag = confirm("Want to delete List ?")
+            if (flag) {
+                e.target.remove();
+            }
+    }
+})
+
+lists.addEventListener("dblclick",(e)=>{
+    e.preventDefault();
+    if(e.target.classList.contains('list-item')){
+        let name = prompt("Enter Name")
+            if (name) {
+                e.target.innerHTML = name;
+            }
+    }
+})
+
+
+
 
 
 input.addEventListener("keydown", (e) => {
-
     if (e.keyCode === 13) {
         button.click();
     }
@@ -23,76 +45,20 @@ button.addEventListener("click", () => {
         div.classList.add("list-item");
         div.draggable = true;
         div.innerHTML = input.value;
-        list[0].appendChild(div);
+        addnewlist.appendChild(div);
         input.value = "";
-        unpdate.disabled = false;
+        
+        updateList();
     }
 })
 
-const unpdate = document.getElementById("update")
-unpdate.addEventListener("click",()=>{
-        updatClick();
-        updatedouble();
-        updateDeletList();
-        updateList();
-        unpdate.disabled = true;
-})
 
 
-function updateDeletList() {
-    listItem = document.querySelectorAll(".list-item")
-    listItem.forEach(list => {
-        list.addEventListener("contextmenu", (e) => {
-            e.preventDefault();
-            let flag = confirm("Want to delete List ?")
-            if (flag) {
-                list.remove();
-            }
-        })
-    })
-}
-
-function updatedouble() {
-    listItem = document.querySelectorAll(".list-item")
-    listItem.forEach(list => {
-        list.addEventListener("dblclick", (e) => {
-            e.preventDefault();
-            let name = prompt("Enter Name")
-            if (name) {
-                list.innerHTML = name;
-            }
-        })
-    })
-}
-
-
-
-function updatClick() {
-    listItem = document.querySelectorAll(".list-item")
-    listItem.forEach(list => {
-        let x = 0;
-        list.addEventListener("click", (e) => {
-            // e.preventDefault();
-            if (e.detail === 1) {
-                if (x >= 2) {
-                    x = -1;
-                }
-                list.style.backgroundColor = color[++x]
-            } else if (e.detail == 2) {
-                list.style.backgroundColor = color[--x]
-            }
-        })
-    })
-}
-
-
-// updatClick();
-// updatedouble();
-// updateDeletList();
 updateList();
 
 function updateList() {
-    listItem = document.querySelectorAll(".list-item")
+    const list = document.querySelectorAll(".list");
+    let listItem = document.querySelectorAll(".list-item");
     listItem.forEach(item => {
         item.addEventListener("dragstart", () => {
             draggedItem = item;
@@ -109,16 +75,32 @@ function updateList() {
             }, 0)
         })
 
+        let x = 0;
+        item.addEventListener("click", (e) => {
+            // e.preventDefault();
+            if (e.detail === 1) {
+                if (x >= 2) {
+                    x = -1;
+                }
+                setTimeout(()=>{
+                item.style.backgroundColor = color[++x]
+            },200)
+            } else if (e.detail == 2) {
+                item.style.backgroundColor = color[--x]
+            }
+        })
+
+
         list.forEach(li => {
             li.addEventListener("dragover", (e) => {
                 e.preventDefault();
                 li.style.backgroundColor = "rgb(3, 110, 105)"
-            });
+            });     
 
             li.addEventListener("dreaenter", (e) => {
                 e.preventDefault();
                 // li.style.backgroundColor = "rgb(3, 110, 105)"
-            });
+            });     
 
             li.addEventListener("dragleave", (e) => {
                 e.preventDefault();
